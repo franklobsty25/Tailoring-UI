392ce250f2e31070f39ff1eb3d969fdf7699fe56
Frank Kodie <franklobsty25@icloud.com>
2021-07-12 08:40:53 +0000

    Added server url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  apiUrl: 'http://localhost:3000'
+  apiUrl: 'http://api.tailoringhub.com:3000'
 };
 
 /*
