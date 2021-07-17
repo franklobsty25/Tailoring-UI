21125734858f0a6410de7386138a4bd16b0ae651
Frank Kodie <franklobsty25@icloud.com>
2021-07-17 18:05:19 +0000

    Remove cart counting

--- src/app/components/common/navbar-style-two/navbar-style-two.component.ts
+++ src/app/components/common/navbar-style-two/navbar-style-two.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { Cart } from '../../../shared/models/cart';
-import { AuthService } from '../../../shared/services/auth.service';
 
 @Component({
     selector: 'app-navbar-style-two',
@@ -9,38 +6,15 @@ import { AuthService } from '../../../shared/services/auth.service';
     styleUrls: ['./navbar-style-two.component.scss']
 })
 export class NavbarStyleTwoComponent implements OnInit {
-    len: number = 0;
-    carts: Cart[];
 
-    constructor(
-        private router: Router,
-        private authService: AuthService
-    ) {}
+    constructor() {}
 
     ngOnInit(): void {
-        this.getCarts();
     }
 
     classApplied = false;
     toggleClass() {
         this.classApplied = !this.classApplied;
-    }
-
-    // get user carts
-    getCarts() {
-        if (this.authService.isLoggedIn) {
-            this.authService.getUserCarts().subscribe(carts => {
-                this.carts = carts;
-                this.len = this.carts.length;
-            },
-            err => {
-                if (err.status === 403)
-                this.router.navigate(['profile-authentication']);
-            });
-        }
-        else {
-            this.len;
-        }
     }
 
 }
